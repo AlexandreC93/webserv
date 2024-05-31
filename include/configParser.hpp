4f118e416ca31acffee5cd10a4a05ff88a0d5f45
Elisabeth ALVES <ealves@bess-f1r1s6.clusters.42paris.fr>
2024-05-31 17:35:38 +0200

    ok

--- include/configParser.hpp
+++ include/configParser.hpp
@@ -1,118 +1,58 @@
-
 #ifndef CONFIG_PARSER_HPP
-# define CONFIG_PARSER_HPP
+#define CONFIG_PARSER_HPP
 
 #include <iostream>
-#include <netinet/in.h>
-#include "configServer.hpp"
-// # include <fstream>
-#include <fcntl.h>
-#include <cstring>
-#include <string>
-#include <unistd.h>
-#include <dirent.h>
-#include <sstream>
-// # include <bits/stdc++.h>
-#include <cstdlib>
 #include <fstream>
-#include <sstream>
-#include <cctype>
-#include <ctime>
-#include <cstdarg>
-// #include containers
+#include <string>
 #include <vector>
 #include <map>
-#include "configServer.hpp"
+#include <limits>
+#include <sstream>
 
-class ConfigParse
-{
-	public:
-		ConfigParse(const std::string &filename);
-		~ConfigParse();
+class LocationBlock {
+public:
+	LocationBlock();
+	~LocationBlock();
 
-		std::vector<Server> getServers() const;
+	std::string location_path;
+	std::string root;
+	std::string index;
+	std::string allow_methods;
+	std::string autoindex;
+	std::string cgi_path;
+	std::string cgi_ext;
+};
 
-	private:
-		std::vector<Server> servers;
-		void parseConfigFile(const std::string &fileName);
-		void parseServerBlock(std::ifstream &ifs, Server &server);
-		void parseLocationBlock(std::ifstream &ifs, Server &server, const std::string &locationPath);
+class ServerBlock {
+public:
+	ServerBlock();
+	~ServerBlock();
+
+	int listen;
+	std::string path;
+	std::string server_name;
+	std::string host;
+	std::string root;
+	std::string index;
+	std::string error_page;
+	std::vector<LocationBlock> locations;
+};
+
+class ConfigParser {
+public:
+	ConfigParser(const std::string& filename);
+	~ConfigParser();
+
+	std::vector<ServerBlock> parseConfig();
+
+private:
+	std::ifstream file;
+	std::vector<ServerBlock> serverBlocks;
+	std::string filename;
+
+	void parseServerBlock();
+	void parseLocationBlock(ServerBlock& serverBlock);
+	std::string getNextToken();
 };
 
 #endif
-
-// serverConfig parseConfig(const std::string &filename)
-// {
-// 	serverConfig sconfig;
-// 	std::ifstream configFile(filename); // ouvre le fichier de config en lecture
-// 	std::string line;					// lis chaque ligne du fichier
-// 	locationConfig currentLoc;			// stock temporairement la config de l'emplacement en cours de traitement
-// 	// bool inLocation = true;			// indique si on est a l'interieur d'un bloc 'location'
-
-// 	while (std::getline(configFile, line)) // lis le fichier ligne par ligne
-// 	{
-// 		std::istringstream iss(line); // creer un flux de chaine a partir de la ligne lue
-// 		std::string key;			  // stock le 1er mot de la ligne, qui est une clef
-// 		iss >> key;					  // lit la clef depuis le flux
-// 		// ignore les lignes contenant server ou location
-// 		if (key == "server")
-// 			continue;
-// 		// stock les keys dans les variables
-// 		if (key == "location")
-// 		{
-// 			std::string location_path;
-// 			std::getline(iss >> std::ws, location_path);
-// 			// Affectation des valeurs appropriées à la nouvelle instance de locationConfig
-// 			currentLoc.file_path = "../config/default.conf";
-// 			sconfig.locations[location_path] = location_path;
-
-// 			std::cout << "Location : " << sconfig.locations[location_path] << std::endl;
-// 			std::cout << "File path : " << currentLoc.file_path << std::endl;
-// 		}
-// 		if (key == "listen")
-// 		{
-// 			iss >> sconfig.listen_port;
-// 			iss.ignore(1, ';');
-// 		}
-// 		else if (key == "server_name")
-// 		{
-// 			iss >> sconfig.server_name;
-// 			removeSemicolon(sconfig.server_name);
-// 		}
-// 		else if (key == "root")
-// 		{
-// 			// verifie si on est dans un bloc 'location'
-// 			iss >> currentLoc.root;
-// 			removeSemicolon(currentLoc.root);
-// 			std::cout << "root : " << currentLoc.root << std::endl;
-// 		}
-// 		else if (key == "index")
-// 		{
-// 			std::string index_file;
-// 			// lis tous les fichiers d'index sur la ligne
-// 			while (iss >> index_file)
-// 			{
-// 				// ajoute le fichier d'index dans la liste idx_files
-// 				currentLoc.idx_files.push_back(index_file);
-// 				removeSemicolon(index_file);
-// 				std::cout << "Index : " << index_file << std::endl;
-// 			}
-// 		}
-// 		else if (key == "error_page")
-// 		{
-// 			std::string error_page;
-// 			while (iss >> error_page)
-// 			{
-// 				std::stringstream ss(error_page);
-// 				int error_code;
-// 				if (ss >> error_code)
-// 				{
-// 					sconfig.err_pages[error_code] = error_page;
-// 					removeSemicolon(sconfig.err_pages[error_code]);
-// 					std::cout << "Err_page : " << sconfig.err_pages[error_code] << std::endl;
-// 				}
-// 			}
-// 		}
-// 	}
-// 	return sconfig;
-// }
--- src/configParser.cpp
+++ src/configParser.cpp
@@ -1,296 +1,128 @@
+
 #include "../include/configParser.hpp"
 
-// std::pair est une structure de donnees qui peut contenir deux elements
-//  de types differents. Elle est definie comme ca :
-// template <typename T1, typename T2>
-// struct pair {
-// T1 first;
-// T2 second;
-// };
+LocationBlock::LocationBlock() {}
+LocationBlock::~LocationBlock() {}
 
-ConfigParse::ConfigParse(const std::string &fileName)
-{
-	parseConfigFile(fileName);
-}
+ServerBlock::ServerBlock() {}
+ServerBlock::~ServerBlock() {}
 
-ConfigParse::~ConfigParse() {};
-
-std::vector<Server> ConfigParse::getServers() const
-{
-	return servers;
-}
-
-void ConfigParse::parseConfigFile(const std::string &fileName)
-{
-	std::ifstream configFile(fileName.c_str());
-	if (!configFile.is_open())
-	{
-		std::cerr << "Could not open config file: " << fileName << std::endl;
-		return;
+ConfigParser::ConfigParser(const std::string& filename) : filename(filename) {
+	file.open(filename.c_str());
+	if (!file.is_open()) {
+		std::cerr << "Erreur lors de l'ouverture du fichier: " << filename << std::endl;
+		exit(EXIT_FAILURE); // Sortie du programme avec erreur
 	}
-	std::string line;
-	while(std::getline(configFile, line))
-	{
-		std::istringstream iss(line);
-		std::string token;
-		iss >> token;
+	ServerBlock server;
+	server.path = filename;
+}
 
-		if (token == "server")
-		{
-			Server server(fileName);
-			while(std::getline(configFile, line))
-			{
-				std::istringstream serverStream(line);
-				serverStream >> token;
-				if (token == "{")
-					break;
-			}
-			parseServerBlock(configFile, server);
-			servers.push_back(server);
+ConfigParser::~ConfigParser() {
+	file.close();
+}
+
+std::vector<ServerBlock> ConfigParser::parseConfig() {
+	std::string token;
+	while ((token = getNextToken()) != "") {
+		if (token == "server") {
+			parseServerBlock();
 		}
 	}
-	configFile.close();
+	return serverBlocks;
 }
 
-void ConfigParse::parseServerBlock(std::ifstream &ifs, Server &server)
-{
-	std::string line;
-	while(std::getline(ifs, line))
-	{
-		std::istringstream serverStream(line);
-		std::string token;
-		serverStream >> token;
-
-		if (token == "}")
-			break;
-		if (token == "listen")
-		{
-			std::string port;
-			serverStream >> port;
-			server.setListenPort(port);
-		}
-		else if (token == "server_name")
-		{
-			std::string name;
-			serverStream >> name;
-			server.setServerName(name);
-		}
-		else if (token == "host")
-		{
-			std::string host;
-			serverStream >> host;
-			server.setHost(host);
-		}
-		else if (token == "root")
-		{
-			std::string root;
-			serverStream >> root;
-			server.setRoot(root);
-		}
-		else if (token == "error_page")
-		{
-			std::string errorPage;
-			std::vector<std::string> err_pages;
-			while (serverStream >> errorPage)
-			{
-				err_pages.push_back(errorPage);
-			}
-			server.setErrPages(err_pages);
-		}
-		else if (token == "location")
-		{
-			std::string locationPath;
-			serverStream >> locationPath;
-			while (std::getline(ifs, line))
-			{
-				std::istringstream locationStream(line);
-				locationStream >> token;
-				if (token == "{")
-					break;
-			}
-			parseLocationBlock(ifs, server, locationPath);
+void ConfigParser::parseLocationBlock(ServerBlock& serverBlock) {
+	LocationBlock locationBlock;
+	std::string token;
+	std::getline(file >> std::ws, locationBlock.location_path, '{'); // Récupère le chemin spécifique
+	while ((token = getNextToken()) != "}") {
+		if (token == "root") {
+			std::getline(file >> std::ws, locationBlock.root, ';');
+		} else if (token == "index") {
+			std::getline(file >> std::ws, locationBlock.index, ';');
+		} else if (token == "allow_methods") {
+			std::getline(file >> std::ws, locationBlock.allow_methods, ';');
+		} else if (token == "autoindex") {
+			std::getline(file >> std::ws, locationBlock.autoindex, ';');
+		} else if (token == "cgi_path") {
+			std::getline(file >> std::ws, locationBlock.cgi_path, ';');
+		} else if (token == "cgi_ext") {
+			std::getline(file >> std::ws, locationBlock.cgi_ext, ';');
 		}
 	}
+	serverBlock.locations.push_back(locationBlock);
 }
 
-void ConfigParse::parseLocationBlock(std::ifstream &ifs, Server &server, const std::string &locationPath)
-{
-	Location location;
-	location.setFilePath(locationPath);
-	std::string line;
-	while (std::getline(ifs, line))
-	{
-		std::istringstream locationStream(line);
-		std::string token;
-		locationStream >> token;
-
-		if (token == "}")
-			break;
-
-		if (token == "root")
-		{
-			std::string root;
-			locationStream >> root;
-			location.setRoot(root, server.getRoot());
+void ConfigParser::parseServerBlock() {
+	ServerBlock serverBlock;
+	std::string token;
+	while ((token = getNextToken()) != "}") {
+		if (token == "listen") {
+			file >> serverBlock.listen;
+		} else if (token == "server_name") {
+			std::getline(file >> std::ws, serverBlock.server_name, ';');
+		} else if (token == "host") {
+			std::getline(file >> std::ws, serverBlock.host, ';');
+		} else if (token == "root") {
+			std::getline(file >> std::ws, serverBlock.root, ';');
+		} else if (token == "index") {
+			std::getline(file >> std::ws, serverBlock.index, ';');
+		} else if (token == "error_page") {
+			std::getline(file >> std::ws, serverBlock.error_page, ';');
+		} else if (token == "location") {
+			parseLocationBlock(serverBlock);
 		}
-		else if (token == "index")
-		{
-			std::vector<std::string> idxFiles;
-			std::string idxFile;
-			while (locationStream >> idxFile)
-			{
-				idxFiles.push_back(idxFile);
-			}
-			location.setIdxFiles(idxFiles);
-		}
-		// Ajoutez d'autres options de configuration pour location ici si nécessaire.
 	}
-	server.setLocation(locationPath, location);
+	serverBlock.path = filename; // Ajout de l'initialisation de la variable path
+	serverBlocks.push_back(serverBlock);
 }
 
-int main()
-{
-    ConfigParse config("../config/default.conf");
-    std::vector<Server> servers = config.getServers();
-
-    for (size_t i = 0; i < servers.size(); ++i)
-    {
-        std::cout << "Server " << i + 1 << ":" << std::endl;
-        std::cout << "Listen Port: " << servers[i].getListenPort() << std::endl;
-        std::cout << "Host: " << servers[i].getHost() << std::endl;
-        std::cout << "Server Name: " << servers[i].getServerName() << std::endl;
-        std::cout << "Root: " << servers[i].getRoot() << std::endl;
-
-        std::cout << "Error Pages: " << std::endl;
-        std::map<int, std::string> errPages = servers[i].getErrPages();
-        for (std::map<int, std::string>::iterator it = errPages.begin(); it != errPages.end(); ++it)
-        {
-            std::cout << it->first << " -> " << it->second << std::endl;
-        }
-
-        std::cout << "Locations: " << std::endl;
-        std::vector<Location> locations = servers[i].getLocations();
-        for (size_t j = 0; j < locations.size(); ++j)
-        {
-            std::cout << "Location " << j + 1 << ":" << std::endl;
-            std::cout << "File Path: " << locations[j].getFilePath() << std::endl;
-            std::cout << "Root: " << locations[j].getRoot() << std::endl;
-            std::cout << "Index Files: ";
-            std::vector<std::string> idxFiles = locations[j].getIdxFiles();
-            for (size_t k = 0; k < idxFiles.size(); ++k)
-            {
-                std::cout << idxFiles[k] << " ";
-            }
-            std::cout << std::endl;
-        }
-    }
-
-    return 0;
+std::string ConfigParser::getNextToken() {
+	std::string token;
+	while (file >> token) {
+		// Vérifier si le jeton commence par '{' ou ';'
+		if (token[0] == '{' || token[0] == ';') {
+			// Ignorer le caractère et continuer
+			file.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+			continue;
+		}
+		// Vérifier si le jeton se termine par ';'
+		if (token[token.size() - 1] == ';') {
+			// Retirer le dernier caractère ';'
+			token = token.substr(0, token.size() - 1);
+		}
+		return token;
+	}
+	return "";
 }
 
-// std::ostream& operator<<(std::ostream& os, const locationConfig& loc)
-// {
-//     os << "file_path: " << loc.file_path << ", root: " << loc.root << ", idx_files: [";
-//     for (size_t i = 0; i < loc.idx_files.size(); ++i)
-//     {
-//         os << loc.idx_files[i];
-//         if (i < loc.idx_files.size() - 1)
-//         {
-//             os << ", ";
-//         }
-//     }
-//     os << "]";
-//     return os;
-// }
 
-// void removeSemicolon(std::string &str)
-// {
-// 	while (!str.empty() && (str.back() == ';' || str.back() == '{' || str.back() == '}'))
-// 	{
-// 		str.pop_back();
-// 	}
-// }
+int main() {
+	ConfigParser parser("../config/default.conf");
+	std::vector<ServerBlock> serverBlocks = parser.parseConfig();
 
-// serverConfig parseConfig(const std::string &filename)
-// {
-// 	serverConfig sconfig;
-// 	std::ifstream configFile(filename); // ouvre le fichier de config en lecture
-// 	std::string line;					// lis chaque ligne du fichier
-// 	locationConfig currentLoc;			// stock temporairement la config de l'emplacement en cours de traitement
-// 	bool inLocation = false;			// indique si on est à l'intérieur d'un bloc 'location'
-// 	std::string currentLocationPath;	// stock le chemin de l'emplacement actuel
+	// Affichage des informations récupérées
+	for (const auto& serverBlock : serverBlocks) {
+		std::cout << "Path: " << serverBlock.path << std::endl;
+		std::cout << "Listen: " << serverBlock.listen << std::endl;
+		std::cout << "Server Name: " << serverBlock.server_name << std::endl;
+		std::cout << "Host: " << serverBlock.host << std::endl;
+		std::cout << "Root: " << serverBlock.root << std::endl;
+		std::cout << "Index: " << serverBlock.index << std::endl;
+		std::cout << "Error Page: " << serverBlock.error_page << std::endl;
 
-// 	while (std::getline(configFile, line)) // lis le fichier ligne par ligne
-// 	{
-// 		std::istringstream iss(line); // creer un flux de chaine a partir de la ligne lue
-// 		std::string key;			  // stock le 1er mot de la ligne, qui est une clef
-// 		iss >> key;					  // lit la clef depuis le flux
-// 		// ignore les lignes contenant server ou location
-// 		if (key == "server")
-// 			continue;
-// 		// stock les keys dans les variables
-// 		if (key == "location")
-// 		{
-// 			inLocation = true;
-// 			std::getline(iss >> std::ws, currentLocationPath);
-// 			currentLoc = locationConfig(); // Réinitialise currentLoc pour la nouvelle section location
-// 			currentLoc.file_path = filename;
-// 			continue; // Passe à la ligne suivante pour traiter l'intérieur du bloc location
-// 		}
-// 		if (key == "}" && inLocation)
-// 		{
-// 			inLocation = false;
-// 			sconfig.locations[currentLocationPath] = currentLoc;
-// 			std::cout << "Location : " << currentLocationPath << std::endl;
-// 			std::cout << "File path: " << currentLoc.file_path << std::endl;
-// 			continue; // Passe à la ligne suivante après avoir fermé un bloc location
-// 		}
-// 		if (inLocation)
-// 		{
-// 			if (key == "root")
-// 			{
-// 				iss >> currentLoc.root;
-// 				removeSemicolon(currentLoc.root);
-// 				std::cout << "root : " << currentLoc.root << std::endl;
-// 			}
-// 			else if (key == "index")
-// 			{
-// 				std::string index_file;
-// 				// lis tous les fichiers d'index sur la ligne
-// 				while (iss >> index_file)
-// 				{
-// 					// ajoute le fichier d'index dans la liste idx_files
-// 					removeSemicolon(index_file);
-// 					currentLoc.idx_files.push_back(index_file);
-// 					std::cout << "Index : " << index_file << std::endl;
-// 				}
-// 			}
-// 		}
-// 		else if (key == "listen")
-// 		{
-// 			iss >> sconfig.listen_port;
-// 			iss.ignore(1, ';');
-// 		}
-// 		else if (key == "server_name")
-// 		{
-// 			iss >> sconfig.server_name;
-// 			removeSemicolon(sconfig.server_name);
-// 		}
-// 		else if (key == "error_page")
-// 		{
-// 			std::string error_page;
-// 			while (iss >> error_page)
-// 			{
-// 				std::stringstream ss(error_page);
-// 				int error_code;
-// 				if (ss >> error_code)
-// 				{
-// 					sconfig.err_pages[error_code] = error_page;
-// 					removeSemicolon(sconfig.err_pages[error_code]);
-// 					std::cout << "Err_page : " << sconfig.err_pages[error_code] << std::endl;
-// 				}
-// 			}
-// 		}
-// 	}
-// 	return sconfig;
-// }
+		std::cout << "Locations:" << std::endl;
+		for (const auto& location : serverBlock.locations) {
+			std::cout << "\tLocation: " << location.location_path << std::endl;
+			std::cout << "\t\tRoot: " << location.root << std::endl;
+			std::cout << "\t\tIndex: " << location.index << std::endl;
+			std::cout << "\t\tAllow Methods: " << location.allow_methods << std::endl;
+			std::cout << "\t\tAutoindex: " << location.autoindex << std::endl;
+			std::cout << "\t\tCGI Path: " << location.cgi_path << std::endl;
+			std::cout << "\t\tCGI Ext: " << location.cgi_ext << std::endl;
+		}
+		std::cout << std::endl;
+	}
+
+	return 0;
+}
