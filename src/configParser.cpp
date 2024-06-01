2bb9fe21040d9c196b38e98c70ff7ecf39d5567b
Elisabeth ALVES <ealves@bess-f2r5s13.clusters.42paris.fr>
2024-06-01 16:56:22 +0200

    ok

--- src/configParser.cpp
+++ src/configParser.cpp
@@ -40,20 +40,28 @@ void ConfigParser::parseLocationBlock(ServerBlock& serverBlock) {
 			std::getline(file >> std::ws, locationBlock.root, ';');
 		} else if (token == "index") {
 			std::getline(file >> std::ws, locationBlock.index, ';');
-		} else if (token == "allow_methods") {
+		} else if (token == "allow_methods" || token == "methods") {
 			std::getline(file >> std::ws, locationBlock.allow_methods, ';');
 		} else if (token == "autoindex") {
 			std::getline(file >> std::ws, locationBlock.autoindex, ';');
+		}else if (token == "upload_store"){
+			std::getline(file >> std::ws, locationBlock.upload_store, ';');
 		} else if (token == "cgi_path") {
 			std::getline(file >> std::ws, locationBlock.cgi_path, ';');
 		} else if (token == "cgi_ext") {
 			std::getline(file >> std::ws, locationBlock.cgi_ext, ';');
+		} else if (token == "cgi"){
+			std::getline(file >> std::ws, locationBlock.cgi, ';');
+		} else if (token == "return")
+		{
+			std::getline(file >> std::ws, locationBlock.ret, ';');
 		}
 	}
 	serverBlock.locations.push_back(locationBlock);
 }
 
-void ConfigParser::parseServerBlock() {
+void ConfigParser::parseServerBlock()
+{
 	ServerBlock serverBlock;
 	std::string token;
 	while ((token = getNextToken()) != "}") {
@@ -67,8 +75,20 @@ void ConfigParser::parseServerBlock() {
 			std::getline(file >> std::ws, serverBlock.root, ';');
 		} else if (token == "index") {
 			std::getline(file >> std::ws, serverBlock.index, ';');
+		} else if (token == "client_max_body_size") {
+			std::getline(file >> std::ws, serverBlock.client_max_body_size, ';');
+		} else if (token == "default_server") {
+			std::getline(file >> std::ws, serverBlock.default_server, ';');
 		} else if (token == "error_page") {
-			std::getline(file >> std::ws, serverBlock.error_page, ';');
+			std::string error_info;
+			std::getline(file >> std::ws, error_info, ';');
+
+			std::istringstream error_stream(error_info);
+			int error_code;
+			std::string error_path;
+			error_stream >> error_code;
+			error_stream >> error_path;
+			serverBlock.error_page[error_code] = error_path;
 		} else if (token == "location") {
 			parseLocationBlock(serverBlock);
 		}
@@ -98,7 +118,7 @@ std::string ConfigParser::getNextToken() {
 
 
 int main() {
-	ConfigParser parser("../config/default.conf");
+	ConfigParser parser("../config/main.conf");
 	std::vector<ServerBlock> serverBlocks = parser.parseConfig();
 
 	// Affichage des informations récupérées
@@ -109,7 +129,12 @@ int main() {
 		std::cout << "Host: " << serverBlock.host << std::endl;
 		std::cout << "Root: " << serverBlock.root << std::endl;
 		std::cout << "Index: " << serverBlock.index << std::endl;
-		std::cout << "Error Page: " << serverBlock.error_page << std::endl;
+		std::cout << "Error Pages:" << std::endl;
+		for (std::map<int, std::string>::const_iterator it_err = serverBlock.error_page.begin(); it_err != serverBlock.error_page.end(); ++it_err) {
+			std::cout << "\t" << it_err->first << ": " << it_err->second << std::endl;
+		}
+		std::cout << "Client size: " << serverBlock.client_max_body_size << std::endl;
+		std::cout << "Default server: " << serverBlock.default_server << std::endl;
 
 		std::cout << "Locations:" << std::endl;
 		for (const auto& location : serverBlock.locations) {
@@ -118,8 +143,11 @@ int main() {
 			std::cout << "\t\tIndex: " << location.index << std::endl;
 			std::cout << "\t\tAllow Methods: " << location.allow_methods << std::endl;
 			std::cout << "\t\tAutoindex: " << location.autoindex << std::endl;
+			std::cout << "\t\tUpload_store: " << location.upload_store << std::endl;
 			std::cout << "\t\tCGI Path: " << location.cgi_path << std::endl;
 			std::cout << "\t\tCGI Ext: " << location.cgi_ext << std::endl;
+			std::cout << "\t\tCGI: " << location.cgi << std::endl;
+			std::cout << "\t\tReturn: " << location.ret << std::endl;
 		}
 		std::cout << std::endl;
 	}
